09a3207f2ce24818432d6d8d86fb6c8c1d300713
Luke <luke@ebarnfarm.com>
2025-05-11 20:21:23 -0500

    Added OLED logic from TemperatureMoniter.ino

--- Arduino/udpServer/udpServer.ino
+++ Arduino/udpServer/udpServer.ino
@@ -1,17 +1,39 @@
+#include <SparkFun_Qwiic_OLED.h>
+#include <res/qw_fnt_8x16.h>
+
+#include <Wire.h>
+#include <string.h>
 #include <WiFi.h>
 #include <WiFiUdp.h>
 
 #define ANALOG_RES 12
+#define DISPLAY_RATE 1000
+#define TEMP_POL_RATE 250
 
-// Replace with your WiFi credentials
 const char* ssid     = "Foe-Glass wifi";
 const char* password = "Wizard_4 wand";
+
+unsigned long sysTime;
+unsigned long lastDispTime;
+unsigned long lastPolTime;
+
+uint8_t l1_posX = 0, l1_posY = 0;
+uint8_t l2_posX = 0, l2_posY = 15;
+uint8_t l3_posX = 0, l3_posY = 30;
+uint8_t l4_posX = 0, l4_posY = 45;
+
+int analogNC = -50;
 
 // Static IP setup
 IPAddress localIP(10, 32, 1, 25);
 IPAddress gateway(10, 32, 0, 1);
 IPAddress subnet(255, 255, 252, 0);
 IPAddress dns(8, 8, 8, 8);
+
+IPAddress ip_adr;
+String ip_str;
+
+Qwiic1in3OLED disp;
 
 WiFiServer tcpServer(6000);      // TCP control
 WiFiClient controlClient;
@@ -27,9 +49,20 @@ unsigned long lastSendTime = 0;
 const unsigned long streamInterval = 20;  // 5ms interval
 
 void setup() {
+  analogReadResolution(12);
+  // Configure I2C bus
+  Wire.setSCL(17);
+  Wire.setSDA(16);
+  Wire.begin();
+
   Serial.begin(115200);
 
-  analogReadResolution(12);
+  if(!disp.begin()) {
+    while(true);
+  }
+
+  disp.text(l1_posX, l1_posY, "Connecting to Wifi");
+  disp.display();
 
   // Set static IP
   WiFi.config(localIP, gateway, subnet, dns);
@@ -41,26 +74,55 @@ void setup() {
     delay(500);
     Serial.print(".");
   }
+
+  ip_adr = WiFi.localIP();
+  ip_str = ip_adr.toString();
+
   Serial.println("\nWiFi connected!");
   Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
+  Serial.println(ip_str);
 
   tcpServer.begin();
   udp.begin(udpPort);
 
   Serial.println("TCP server on port 6000");
   Serial.println("UDP streaming on port 5000");
+
+  disp.erase();
+  disp.text(l1_posX, l1_posY, "Connected");
+  disp.text(l2_posX, l2_posY, ip_str);
+  disp.display();
+  
+  delay(500);
+  disp.erase();
+  disp.setFont(QW_FONT_8X16);
 }
 
 void loop() {
+  float temperature, humidity;
+
+  String l1_text = "Temp A: ";
+  String l2_text = "RH: ";
+  String l3_text = "Temp 1: ";
+  String l4_text = "Temp 2: ";
+
+  float tempCH0 = convertTemp_F(analogRead(A0));
+  float tempCH1 = convertTemp_F(analogRead(A1));
+  float tempCH2 = convertTemp_F(analogRead(A2));
+
+  unsigned long sysTime = millis();
+
+  char jsonPacket[128];
+  int packetLen;
+
   // Handle TCP control
   if (!controlClient || !controlClient.connected()) {
-    controlClient = tcpServer.available();
+    controlClient = tcpServer.accept();
   }
 
   if (controlClient && controlClient.available()) {
-    int len = controlClient.readBytesUntil('\n', tcpBuffer, sizeof(tcpBuffer) - 1);
-    tcpBuffer[len] = '\0';
+    packetLen = controlClient.readBytesUntil('\n', tcpBuffer, sizeof(tcpBuffer) - 1);
+    tcpBuffer[packetLen] = '\0';
     Serial.print("[TCP] Command: ");
     Serial.println(tcpBuffer);
 
@@ -77,23 +139,42 @@ void loop() {
 
   // Stream data over UDP
   if (streaming) {
-    unsigned long now = millis();
-    if (now - lastSendTime >= streamInterval) {
-      float analogValue = convertTemp_F(analogRead(A0));
-
-      char jsonPacket[128];
+    if (sysTime - lastSendTime >= streamInterval) {
       snprintf(jsonPacket, sizeof(jsonPacket),
         "{\"type\":\"CIP\",\"tag\":\"analogValue\",\"value\":%f,\"timestamp\":%lu}",
-        analogValue, now);
+        tempCH0, sysTime);
 
       udp.beginPacket(clientIP, clientUDPPort);
       udp.write(jsonPacket);
       udp.endPacket();
 
       Serial.println(jsonPacket);
-      lastSendTime = now;
+      lastSendTime = sysTime;
     }
   }
+
+  // Local display logic
+  if (tempCH0 > analogNC) {
+    l3_text+=String(tempCH0);
+  } else {
+    l3_text+="--.--";
+  }
+  if(tempCH1 > analogNC) {
+    l4_text+=String(tempCH1);
+  } else {
+    l4_text+="--.--";
+  }
+
+  if(sysTime > lastDispTime+DISPLAY_RATE) {
+    lastDispTime = sysTime; 
+    disp.erase();
+
+    disp.text(l1_posX, l1_posY, l1_text);
+    disp.text(l2_posX, l2_posY, l2_text);
+    disp.text(l3_posX, l3_posY, l3_text);
+    disp.text(l4_posX, l4_posY, l4_text);
+    disp.display();
+  }      
 }
 
 float convertTemp_F(uint16_t AInput) {
